967cebd750ff530922d542fa996ee597abf2c8f1
Chase Tucker <tuck8106@gmail.com>
2019-10-15 14:09:27 -0700

    committed before chart

--- js/app.js
+++ js/app.js
@@ -182,6 +182,27 @@ function displayResults() {
     figure.appendChild(figcaption);
     imageContainer.appendChild(figure);
   }
+
+  // chart
+  var context = document.getElementById('chart').getContext('2d');
+  var chart = new Chart(context, {
+      // The type of chart we want to create
+      type: 'line',
+
+      // The data for our dataset
+      data: {
+          labels: ['January', 'February', 'March', 'April', 'May', 'June', 'July'],
+          datasets: [{
+              label: 'My First dataset',
+              backgroundColor: 'rgb(255, 99, 132)',
+              borderColor: 'rgb(255, 99, 132)',
+              data: [0, 10, 5, 2, 20, 30, 45]
+          }]
+      },
+
+      // Configuration options go here
+      options: {}
+  });
   console.log(products.products);
 }
 
